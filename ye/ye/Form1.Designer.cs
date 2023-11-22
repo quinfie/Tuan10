e5eefb509a39c2e313759d43b403d8c2ed26e87e
quinfie <dltquynh30@gmail.com>
2023-11-22 14:01:57 +0700

    add form quiz

--- ye/ye/Form1.Designer.cs
+++ ye/ye/Form1.Designer.cs
@@ -28,11 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBTN_D = new System.Windows.Forms.RadioButton();
+            this.radioBTN_C = new System.Windows.Forms.RadioButton();
+            this.radioBTN_B = new System.Windows.Forms.RadioButton();
+            this.radioBTN_A = new System.Windows.Forms.RadioButton();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(599, 321);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBTN_D);
+            this.groupBox1.Controls.Add(this.radioBTN_C);
+            this.groupBox1.Controls.Add(this.radioBTN_B);
+            this.groupBox1.Controls.Add(this.radioBTN_A);
+            this.groupBox1.Location = new System.Drawing.Point(100, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 131);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioBTN_D
+            // 
+            this.radioBTN_D.AutoSize = true;
+            this.radioBTN_D.Location = new System.Drawing.Point(261, 73);
+            this.radioBTN_D.Name = "radioBTN_D";
+            this.radioBTN_D.Size = new System.Drawing.Size(85, 17);
+            this.radioBTN_D.TabIndex = 2;
+            this.radioBTN_D.TabStop = true;
+            this.radioBTN_D.Text = "radioButton1";
+            this.radioBTN_D.UseVisualStyleBackColor = true;
+            // 
+            // radioBTN_C
+            // 
+            this.radioBTN_C.AutoSize = true;
+            this.radioBTN_C.Location = new System.Drawing.Point(261, 41);
+            this.radioBTN_C.Name = "radioBTN_C";
+            this.radioBTN_C.Size = new System.Drawing.Size(85, 17);
+            this.radioBTN_C.TabIndex = 3;
+            this.radioBTN_C.TabStop = true;
+            this.radioBTN_C.Text = "radioButton1";
+            this.radioBTN_C.UseVisualStyleBackColor = true;
+            // 
+            // radioBTN_B
+            // 
+            this.radioBTN_B.AutoSize = true;
+            this.radioBTN_B.Location = new System.Drawing.Point(57, 73);
+            this.radioBTN_B.Name = "radioBTN_B";
+            this.radioBTN_B.Size = new System.Drawing.Size(85, 17);
+            this.radioBTN_B.TabIndex = 4;
+            this.radioBTN_B.TabStop = true;
+            this.radioBTN_B.Text = "radioButton1";
+            this.radioBTN_B.UseVisualStyleBackColor = true;
+            // 
+            // radioBTN_A
+            // 
+            this.radioBTN_A.AutoSize = true;
+            this.radioBTN_A.Location = new System.Drawing.Point(57, 41);
+            this.radioBTN_A.Name = "radioBTN_A";
+            this.radioBTN_A.Size = new System.Drawing.Size(85, 17);
+            this.radioBTN_A.TabIndex = 5;
+            this.radioBTN_A.TabStop = true;
+            this.radioBTN_A.Text = "radioButton1";
+            this.radioBTN_A.UseVisualStyleBackColor = true;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(228, 247);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(143, 46);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "CÂU TIẾP THEO";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(98, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TRẮC NGHIỆM TRÍ KHÔN TA ĐÂY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(606, 328);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBTN_D;
+        private System.Windows.Forms.RadioButton radioBTN_C;
+        private System.Windows.Forms.RadioButton radioBTN_B;
+        private System.Windows.Forms.RadioButton radioBTN_A;
     }
 }
--- ye/ye/Form1.cs
+++ ye/ye/Form1.cs
@@ -12,9 +12,87 @@ namespace ye
 {
     public partial class Form1 : Form
     {
+        private List<Question> questions;
+        private int currentQuestionIndex;
         public Form1()
         {
             InitializeComponent();
+
+            questions = new List<Question>
+            {
+                new Question("Câu hỏi 1", new List<string> { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D" }, 0),
+                new Question("Câu hỏi 2", new List<string> { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D" }, 1),
+                new Question("Câu hỏi 3", new List<string> { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D" }, 2),
+                new Question("Câu hỏi 4", new List<string> { "Đáp án A", "Đáp án B", "Đáp án C", "Đáp án D" }, 3)          
+            };
+
+            currentQuestionIndex = 0;
+            DisplayQuestion();
+        }
+        private void DisplayQuestion()
+        {
+            if (currentQuestionIndex >= 0 && currentQuestionIndex < questions.Count)
+            {
+                Question currentQuestion = questions[currentQuestionIndex];
+                label1.Text = currentQuestion.Content;
+
+                radioBTN_A.Text = currentQuestion.Answers[0];
+                radioBTN_B.Text = currentQuestion.Answers[1];
+                radioBTN_C.Text = currentQuestion.Answers[2];
+                radioBTN_D.Text = currentQuestion.Answers[3];
+            }
+        }
+
+         private void nextButton_Click_1(object sender, EventArgs e)
+         {
+             int selectedAnswer = -1;
+             if (radioBTN_A.Checked)
+                 selectedAnswer = 0;
+             else if (radioBTN_B.Checked)
+                 selectedAnswer = 1;
+             else if (radioBTN_C.Checked)
+                 selectedAnswer = 2;
+             else if (radioBTN_D.Checked)
+                 selectedAnswer = 3;
+
+             if (selectedAnswer == questions[currentQuestionIndex].CorrectAnswer)
+             {
+                 MessageBox.Show("Câu trả lời đúng!");
+             }
+             else
+             {
+                 MessageBox.Show("Câu trả lời sai!");
+             }
+
+             // Chuyển đến câu hỏi tiếp theo
+             currentQuestionIndex++;
+             if (currentQuestionIndex < questions.Count)
+             {
+                 DisplayQuestion();
+             }
+             else
+             {
+                 MessageBox.Show("Bạn đã hoàn thành trắc nghiệm!");
+             }
+         }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+
+         }
+
+    }
+    public class Question
+    {
+        public string Content { get; set; }
+        public List<string> Answers { get; set; }
+        public int CorrectAnswer { get; set; }
+
+        public Question(string content, List<string> answers, int correctAnswer)
+        {
+            Content = content;
+            Answers = answers;
+            CorrectAnswer = correctAnswer;
         }
     }
 }
